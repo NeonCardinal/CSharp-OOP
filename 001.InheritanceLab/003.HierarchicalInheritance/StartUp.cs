4415a28b9f68ee458372bf418eb9a23422188173
NeonCardinal <nvnnaidenov@gmail.com>
2022-07-02 20:18:15 +0300

    add

---
+++ 001.InheritanceLab/003.HierarchicalInheritance/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+
+            cat.Meow();
+            dog.Bark();
+        }
+    }
+}
