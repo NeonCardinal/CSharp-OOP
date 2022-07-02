22931b2bb3223339d0244612c955e40bf8c3f1ac
NeonCardinal <nvnnaidenov@gmail.com>
2022-07-02 20:17:51 +0300

    add

---
+++ 001.InheritanceLab/002.MultipleInheritance/StartUp.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Farm
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            Dog dog = new Dog();
+            Puppy puppy = new Puppy();
+
+            dog.Eat();
+            dog.Bark();
+
+            puppy.Eat();
+            puppy.Bark();
+            puppy.Weep();
+        }
+    }
+}
