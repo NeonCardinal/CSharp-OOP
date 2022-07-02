a30aa5a44c65a0b17663fc8f90c188af9b075dd9
NeonCardinal <nvnnaidenov@gmail.com>
2022-07-02 20:18:38 +0300

    add

---
+++ 001.InheritanceLab/004.RandomList/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            RandomList list = new RandomList();
+
+            list.Add("test");
+            list.Add("test2");
+            list.Add("test3");
+        }
+    }
+}
