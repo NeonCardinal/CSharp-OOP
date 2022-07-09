70f0c6ff49837e82a954c848e624f19b5abb9be7
NeonCardinal <nvnnaidenov@gmail.com>
2022-07-09 15:46:35 +0300

    add

---
+++ 002.InheritanceExercise/001.Person/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public  class Person
+    {
+        private int age;
+        private string name;
+
+        public int Age
+        { 
+            get { return age; } 
+            set 
+            {
+                if(value < 0)
+                    value = 0;
+                age = value; 
+            } 
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
