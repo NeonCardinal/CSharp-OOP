2c8641ff30e9debc231dcb7d8ff85a514d405135
NeonCardinal <nvnnaidenov@gmail.com>
2022-07-09 15:47:28 +0300

    add

---
+++ 002.InheritanceExercise/003.PlayersAndMonsters/Hero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PlayersAndMonsters
+{
+    public class Hero
+    {
+        private int level;
+        private string username;
+
+        public int Level
+        {
+            get { return level; }
+            set { level = value; }
+        }
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public Hero(string username, int level)
+        {
+            this.Username = username;
+            this.Level = level;
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name} Username: {this.Username} Level: {this.Level}";
+        }
+    }
+}
