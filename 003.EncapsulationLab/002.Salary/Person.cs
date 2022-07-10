f3347976130526ead03ee4a5f753caf94842c3f1
NeonCardinal <nvnnaidenov@gmail.com>
2022-07-10 12:16:14 +0300

    add

---
+++ 003.EncapsulationLab/002.Salary/Person.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            private set
+            {
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            private set
+            {
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get
+            {
+                return salary;
+            }
+            set
+            {
+                salary = value;
+            }
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percent)
+        {
+            if(this.Age > 30)
+            {
+                this.Salary += this.Salary * percent / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percent / 200;
+            }
+        }
+    }
+}
---
+++ 003.EncapsulationLab/002.Salary/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> persons = new List<Person>();
+
+            for(int i = 0; i < n; i++)
+            {
+                var strs = Console.ReadLine().Split();
+                Person person = new Person(strs[0], strs[1], int.Parse(strs[2]), decimal.Parse(strs[3]));
+                persons.Add(person);
+            }
+
+            decimal percentage = decimal.Parse(Console.ReadLine());
+
+            persons.ForEach(p => p.IncreaseSalary(percentage));
+            persons.ForEach(p => Console.WriteLine(p.ToString()));
+        }
+    }
+}
